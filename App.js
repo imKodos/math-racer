33e6607cc4613181c7b3a4cd21cb1f053a197c01
imkodos <notnek4@gmail.com>
2022-03-01 22:33:07 -0500

    keyboard autofocus and todo log

--- App.js
+++ App.js
@@ -6,10 +6,14 @@ export default function App() {
   const [input, setInput] = React.useState("");
 
   // alert(`message: ${input}`);
-
+//todo look at https://www.freecodecamp.org/news/how-to-make-your-react-native-app-respond-gracefully-when-the-keyboard-pops-up-7442c1535580/
+//todo idea: have question at top of screen, input at bottom of screen, racer in middle.
+//have input grey while typing, green when correct, red when wrong.
+//if wrong, create obstacle?
+//if wrong, next input clears whole textinput, or just doesnt allow the wrong digit?
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!!</Text>
+      <Text>{`4 + 4`}</Text>
       <StatusBar style="auto" />
       <TextInput
         style={styles.input}
@@ -17,6 +21,7 @@ export default function App() {
         value={input}
         keyboardType="number-pad"
         keyboardAppearance="dark"
+        // autoFocus={true}
       ></TextInput>
     </View>
   );
@@ -28,11 +33,11 @@ const styles = StyleSheet.create({
     borderColor: "gray",
     borderWidth: 1,
     borderRadius: 20,
-    padding: 10,
+    padding: 10
   },
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    backgroundColor: "dodgerblue",
     alignItems: "center",
     justifyContent: "center",
   },
