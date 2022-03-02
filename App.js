9febe8762ff59954cba2169471108aa321103a54
Ken <notnek4@gmail.com>
2022-03-01 19:27:06 -0500

    added keyboard

--- App.js
+++ App.js
@@ -1,16 +1,35 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import { StyleSheet, Text, View, TextInput } from "react-native";
 
 export default function App() {
+  const [input, setInput] = React.useState("");
+
+  // alert(`message: ${input}`);
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!!</Text>
       <StatusBar style="auto" />
+      <TextInput
+        style={styles.input}
+        onChangeText={(text) => setInput(text)}
+        value={input}
+        keyboardType="number-pad"
+        keyboardAppearance="dark"
+      ></TextInput>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  input: {
+    width: 200,
+    borderColor: "gray",
+    borderWidth: 1,
+    borderRadius: 20,
+    padding: 10,
+  },
   container: {
     flex: 1,
     backgroundColor: "#fff",
