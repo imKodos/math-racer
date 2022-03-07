a580c79c330030c7252b4436d3d1635ead98cfd5
imkodos <notnek4@gmail.com>
2022-03-07 01:05:29 -0500

    comments for addtnl style

--- App.js
+++ App.js
@@ -50,6 +50,29 @@ export default function App() {
 }
 
 const styles = StyleSheet.create({
+//   input: {
+//     width: 280,
+//     borderColor: "white",
+//     borderWidth: 2,
+//     borderRadius: 10,
+//     padding: 10,
+//     marginLeft: 3,
+//     marginVertical:-2
+//   },
+//   outerInput1: {
+//     width: 300,
+//     borderColor: "cyan",
+//     borderWidth: 2,
+//     borderRadius: 10,
+//   },
+//   outerInput2: {
+//     width: 290,
+//     borderColor: "hotpink",
+//     borderWidth: 2,
+//     borderRadius: 10,
+//     marginLeft:3,
+//     marginVertical:-2
+//   },
   input: {
     width: 308,
     borderColor: "white",
